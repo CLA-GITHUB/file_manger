234effc49ec33e134659f3c8d62e3e3abc68f0f5
cla-github <christwealthla@gmail.com>
2025-05-21 07:51:37 -0700

    chore: rearranged file structure: aiming for a feature-based folder structure

--- api/routes.go
+++ api/routes.go
@@ -1,20 +1,26 @@
 package api
 
 import (
-	"github.com/cla-github/file_manager/api/handlers"
 	"github.com/cla-github/file_manager/api/middlewares"
+	"github.com/cla-github/file_manager/internal/user/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/ping", handlers.Ping)
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"error": nil,
+			"data":  "Pong...",
+		})
+	})
 
-	router.POST("/auth", handlers.Signin)
-	router.POST("/auth/create-account", handlers.Signup)
+	router.POST("/auth", user.Signin)
+	router.POST("/auth/create-account", user.Signup)
 
 	// testing protected middleware route
-	router.GET("/me", middlewares.AuthMiddleware(), handlers.Me)
+	router.GET("/me", middlewares.AuthMiddleware(), user.Me)
+
 	return router
 }
